bac882fdf8afc81c8eb3a77d29b0875a97d6dff3
Rodrigo Feijó <rodrigofeijo.bsi@gmail.com>
2023-01-13 16:05:27 -0300

    Break the app on purpose

--- cypress/integration/google.spec.js
+++ cypress/integration/google.spec.js
@@ -1,4 +1,4 @@
-it('serasa dev', function() {
+it('teste com página na web', function() {
     cy.visit('https://google.com/')
     cy.get('input[type="text"]').type('Parana Clube')
 })  
--- cypress/integration/privacy.spec.js
+++ cypress/integration/privacy.spec.js
@@ -1,5 +1,5 @@
 
-it.only('testa a página da política de privacidade de forma independente', function() {
+it('testa a página da política de privacidade de forma independente', function() {
     cy.visit('./src/privacy.html')
     cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT - Política de privacidade')
     cy.contains('Talking About Testing').should('be.visible')
